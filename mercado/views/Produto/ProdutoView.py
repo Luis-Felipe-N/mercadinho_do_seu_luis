01b7c6424e93b9fae11a4a629dad405f9a4a3b4c
Luis-Felipe-N <luisj2felipe09@gmail.com>
2022-04-08 20:57:35 -0300

    change product view

--- mercado/views/Produto/ProdutoView.py
+++ mercado/views/Produto/ProdutoView.py
@@ -1,12 +1,12 @@
-from django.views import View
-from django.shortcuts import render
+from django.views.generic import DetailView
+from django.shortcuts import get_object_or_404, render
 from mercado.models.Produto import Produto
 
-class ProdutoView(View):
+class ProdutoView(DetailView):
     template_name = "mercado/pages/produto-detalhe.html"
     
     def get(self, request, id):
-        produto = Produto.objects.get(pk=id)
+        produto = get_object_or_404(Produto, pk=id)
 
         print(produto.vendendor)
 
