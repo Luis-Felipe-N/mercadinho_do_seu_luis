35241461ab30a95da6f0c663d44b167ee55dd661
Luis-Felipe-N <luisj2felipe09@gmail.com>
2022-04-13 20:38:12 -0300

    set env in setting s

--- mercadinho_do_seu_luis/settings.py
+++ mercadinho_do_seu_luis/settings.py
@@ -1,5 +1,10 @@
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+from utils.environment import get_env_variable, parse_comma_sep_str_to_list
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,12 +14,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'INSECURE')
+SECRET_KEY = os.getenv('SECRET_KEY', 'INSECURE')
 
-DEBUG = False if os.environ.get('DEBUG') == '0' else True
+DEBUG = True if os.environ.get('DEBUG') == '1' else False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = parse_comma_sep_str_to_list(
+    get_env_variable('ALLOWED_HOSTS')
+)
 
+CSRF_TRUSTED_ORIGINS = parse_comma_sep_str_to_list(
+    get_env_variable('CSRF_TRUSTED_ORIGINS')
+)
 
 # Application definition
 
