eabb58f85071687518670c29525dfc78265e7808
Luis-Felipe-N <luisj2felipe09@gmail.com>
2022-04-26 22:33:59 -0300

    'csrf'

--- mercadinho_do_seu_luis/settings.py
+++ mercadinho_do_seu_luis/settings.py
@@ -16,10 +16,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', 'INSECURE')
 
-DEBUG = True
+DEBUG = True if os.environ.get('DEBUG') == '1' else False
 
 ALLOWED_HOSTS = parse_comma_sep_str_to_list(
     get_env_variable('ALLOWED_HOSTS')
+)
+
+CSRF_TRUSTED_ORIGINS: list[str] = parse_comma_sep_str_to_list(
+    get_env_variable('CSRF_TRUSTED_ORIGINS')
 )
 
 # Application definition
