d493fbb118dd7e241d92a0926f7096e2e88b2b03
Luis-Felipe-N <luisj2felipe09@gmail.com>
2022-05-01 10:38:29 -0300

    debug

--- mercadinho_do_seu_luis/settings.py
+++ mercadinho_do_seu_luis/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', 'INSECURE')
 
-DEBUG = True
+DEBUG = True if os.getenv('DEBUG') == '1' else False
 
 ALLOWED_HOSTS = parse_comma_sep_str_to_list(
     get_env_variable('ALLOWED_HOSTS')
